27187c0a4911d0ac5ceab97b43aa834abac0ef61
annatorok <mail@annatorok.com>
2016-07-18 14:28:56 +0200

    Add mysql server and get module

--- server.js
+++ server.js
@@ -1,9 +1,58 @@
+'use strict';
+
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.use(bodyParser.json());
-// app.use(express.static('../../../week-08/day-4/todo_api'));
+app.use(express.static('client'));
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'calories'
+});
+
+con.connect(function(err){
+  if(err){
+    console.log("Error connecting to Db");
+    return;
+  }
+  console.log("Connection established");
+});
+
+function errorHandling (err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+}
+
+app.get('/meals', function(req, res) {
+  con.query('SELECT * FROM meal_calories', function (err, result) {
+       if (err) {
+            console.log(err);
+            return;
+  }
+        res.send(result);
+})
+});
+
+app.post('/meals', urlencodedParser, function(req, res) {
+  con.query("INSERT INTO meal_calories (name, calories, date) VALUES ('" + req.body.text + '' + '' + "')", function (err, result) {
+    if (err) {
+            console.log(err);
+      return;
+    }
+    res.send({ id: result.insertId, text: req.body.text });
+  });
+});
+
+
+
 
 app.listen(3000);
